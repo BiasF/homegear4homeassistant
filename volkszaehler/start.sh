72885b945930d5fa433bdb67e22d6966ac68ce94
BiasF <tobias.friede@wki.fraunhofer.de>
2021-09-02 22:53:33 +0200

    asc
    sdc

---
+++ volkszaehler/start.sh
@@ -0,0 +1,19 @@
+#/bin/bash
+
+
+if ! [ -d "/config/vzlogger" ]
+then
+	mkdir /config/vzlogger
+	echo "Create config dir"
+fi
+
+if ! [ -d "/share/vzlogger" ]
+then
+	mkdir /config/vzlogger
+	echo "Create config dir"
+fi
+
+
+
+echo hallo
+while :; do :; done & kill -STOP $! && wait $!
