c89ae390b40918533c1a90736ba149f61d972532
BiasF <tobias.friede@wki.fraunhofer.de>
2021-09-02 19:51:07 +0200

    asdc

--- vzlogger/start.sh
+++ vzlogger/start.sh
@@ -7,5 +7,13 @@ then
 	echo "Create config dir"
 fi
 
+if ! [ -d "/share/vzlogger" ]
+then
+	mkdir /config/vzlogger
+	echo "Create config dir"
+fi
+
+
+
 echo hallo
 while :; do :; done & kill -STOP $! && wait $!
